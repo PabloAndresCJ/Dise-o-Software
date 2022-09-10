2a3876528af4859baa6eea169fd57a60b2e0c956
jhulipar <jhullipar@gmail.com>
2022-09-10 19:25:23 -0400

    Router Links funcionando
    Falta encontrar algún reemplazo al album de bootstrap para react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Header';
-import {Route,Routes} from 'react-router-dom';
+import {Route,BrowserRouter,Routes} from 'react-router-dom';
 import { Carousel } from 'bootstrap';
 import Carousel1 from './Components/Carousel1';
+import Deportivo from './Components/Deportivo';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Carousel1/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Carousel1/>}/>
+          <Route path='/Deportivos' element={<Deportivo/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app/src/Components/Deportivo.js
@@ -0,0 +1,22 @@
+import '../assets/dist/css/bootstrap.min.css';
+function Deportivo() {
+    return ( 
+    <html>
+    <head>
+        <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet"/>
+
+    </head>
+    <body>
+    <main>
+        <div style="text-align: center;">
+            <img src="../img/deporte-Ajuste.jpg" class="d-block w-100 d-block h-10" height="600"/>
+            <div class="centrado"><h1 style="color: white; font-size: 100px" >In-Ear</h1></div>
+        </div>
+    
+        
+    </main>
+    </body>
+    </html>);
+}
+
+export default Deportivo;
