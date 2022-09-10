3bf472912ea91d4972577f8e63ed405fbfefa697
jhulipar <jhullipar@gmail.com>
2022-09-09 21:32:21 -0300

    Carousel en React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Header';
+import {Route,Routes} from 'react-router-dom';
+import { Carousel } from 'bootstrap';
+import Carousel1 from './Components/Carousel1';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Carousel1/>
     </div>
   );
 }
