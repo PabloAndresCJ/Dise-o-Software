4ddb6042e0620dab9cbaee55ff86bfa70c308863
Jhul Ipar <jhiparraguirre@alumnos.uai.cl>
2022-11-12 19:25:37 -0300

    Barra de busqueda
    instalar
    @material-ui/core
    @material-ui/icons

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
-
+import Data1 from './Data1.json';
+import SearchBar from './Searchbar';
 const Navbar = () => {
     const {user, isAuthenticated} = useAuth0()
 
@@ -77,24 +78,15 @@ const Navbar = () => {
                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                                     </svg> Contactanos</a>
                             </li>
-
                             <li class='nav-item'>
                                 {logeado()}
                             </li>
-
-                        </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Buscar producto..." aria-label="Search"/>
-
-
-
-                            <button class="btn btn-outline-light" type="submit" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                            </svg></button>
-                        </form>
+                        </ul>  
                     </div>
                 </div>
+                <form class="search-form"><SearchBar placeholder={"Buscar Productos"} data={Data1}/></form>
             </nav>
+            
         </header>
     );
 }
